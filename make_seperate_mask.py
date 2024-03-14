d57b41853c22e8953a09cf85aaeceba73b305813
jh79783 <jh79783@naver.com>
2024-03-14 12:07:47 +0900

    getitem to function

--- make_seperate_mask.py
+++ make_seperate_mask.py
@@ -5,11 +5,11 @@ import numpy as np
 
 class Image:
     def __init__(self, img_path, save_path):
-        self.img_list = [f for f in os.listdir(img_path) if f[-3:] == 'jpg']
+        self.img_list = [f for f in os.listdir(img_path) if f[-3:] in ['png', 'jpg']]
         self.img_path = img_path
         self.save_path = save_path
 
-    def get_contour(self, img_name=None, file_name=None):
+    def get_contour(self, img_name=None):
         if img_name:
             img = cv2.imread(img_name)
             _, thr = cv2.threshold(img, 70, 255, cv2.THRESH_BINARY)
@@ -34,22 +34,36 @@ class Image:
                             f.write(f'{j[0][0]}, {j[0][1]}\n')
             return result
 
-    def __getitem__(self, idx):
-        img = cv2.imread(os.path.join(self.img_path, self.img_list[idx]), 0)
-        mask_img = {}
-        for object_number in np.unique(img):
-            mask_img[object_number] = np.zeros_like(img)
-        for i in range(len(img)):
-            for j in range(len(img[0])):
-                object_num = img[i, j]
-                mask_img[object_num][i, j] = 255
-        for key in mask_img.keys():
-            cv2.imwrite(os.path.join(self.save_path, f'{self.img_list[idx].split(".")[0]}_{key}.jpg'), mask_img[key])
+    def make_seperate_mask(self, idx=None):
+        if idx:
+            img = cv2.imread(os.path.join(self.img_path, self.img_list[idx]), 0)
+            mask_img = {}
+            for object_number in np.unique(img):
+                mask_img[object_number] = np.zeros_like(img)
+            for i in range(len(img)):
+                for j in range(len(img[0])):
+                    object_num = img[i, j]
+                    mask_img[object_num][i, j] = 255
+            for key in mask_img.keys():
+                cv2.imwrite(os.path.join(self.save_path, f'{self.img_list[idx].split(".")[0]}_{key}.jpg'), mask_img[key])
+        else:
+            for img_name in self.img_list:
+                img = cv2.imread(os.path.join(self.img_path, img_name), 0)
+                mask_img = {}
+                for object_number in np.unique(img):
+                    mask_img[object_number] = np.zeros_like(img)
+                for i in range(len(img)):
+                    for j in range(len(img[0])):
+                        object_num = img[i, j]
+                        mask_img[object_num][i, j] = 255
+                for key in mask_img.keys():
+                    cv2.imwrite(os.path.join(self.save_path, f'{img_name.split(".")[0]}_{key}.jpg'), mask_img[key])
 
 
 def main():
-    img_path = '.'
+    img_path = './mask_data'
     image = Image(img_path, './label_data')
+    # image.make_seperate_mask()
     image.get_contour()
 
 
